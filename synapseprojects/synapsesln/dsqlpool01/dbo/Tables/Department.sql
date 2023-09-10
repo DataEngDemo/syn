9c77b46babb6a04a4a1583d35636bfa5beb08ada
kalyan-dev <51482449+kalyan-dev@users.noreply.github.com>
2023-09-10 19:51:28 +0530

    dsql pool tables created

---
+++ synapseprojects/synapsesln/dsqlpool01/dbo/Tables/Department.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Department] (
+    [id]   INT          NULL,
+    [name] VARCHAR (20) NULL
+)
+WITH (HEAP, DISTRIBUTION = ROUND_ROBIN);
+
---
+++ synapseprojects/synapsesln/dsqlpool01/dbo/Tables/Employee.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [id]     INT           NULL,
+    [name]   VARCHAR (100) NULL,
+    [deptId] INT           NULL
+)
+WITH (HEAP, DISTRIBUTION = ROUND_ROBIN);
+
